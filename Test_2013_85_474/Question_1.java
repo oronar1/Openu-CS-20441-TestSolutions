fa936ddfdfc98c33181a9b89a78d5df2e06acbc4
oronar1 <oronara@gmail.com>
2017-02-28 20:28:42 +0200

    Update Question_1.java

--- Test_2013_85_474/Question_1.java
+++ Test_2013_85_474/Question_1.java
@@ -1,5 +1,18 @@
 package Test_2013_85_474;
 
 public class Question_1 {
+public int longestAscending()
+    {
+        return longestAscending(_head, 0);
+    }
+    
+    public int longestAscending(IntNode ind, int count)
+    {
+        if(ind == null || ind.getNext() == null)
+            return count;
+        if(ind.getNext().getValue() > ind.getValue())
+            return longestAscending(ind.getNext(), count+1);
+        return longestAscending(ind.getNext(), count);
+    }
 
 }
